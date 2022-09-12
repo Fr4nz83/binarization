70d88f5b922c5033c59cc9bf5dfe400c72eeacad
Fr4nz83 <francesco.lettich@gmail.com>
2022-09-12 17:34:30 +0200

    Merge branch 'main' of https://github.com/Fr4nz83/binarization.git into main

---
+++ src/layers/util_layers.cuh
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../generator.h"
+#include "layer_MLPMixer.cuh"
+
+BatchNormFullPrecLayer::BatchNormLayerParams generate(const char* name,
+													  const unsigned& in_width,
+													  const unsigned& in_height,
+													  const unsigned& in_channels)
+{
+	float *scale = gen_matrix_ptr(1, in_channels);
+	float *shift = gen_matrix_ptr(1, in_channels);
+
+	return {name, in_width, in_height, in_channels, scale, shift};
+}
